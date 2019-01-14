31c2cd50d019543c7101f43a1b8ed6b2bedec66f
shanhm <shanhm1991@163.com>
2019-01-14 13:05:20 +0800

    examples测试完成

--- src/main/java/com/fom/boot/ApplicationConfiguration.java
+++ src/main/java/com/fom/boot/ApplicationConfiguration.java
@@ -3,11 +3,13 @@ package com.fom.boot;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 import com.fom.context.config.ConfigListener;
 import com.fom.context.db.pool.PoolListener;
 
 @Configuration
+@ImportResource(locations= {"WEB-INF/springContext.xml"})
 public class ApplicationConfiguration {
 	
 	@Bean
