b1ec9cdd9cf2b3b516e107a403976dba82931720
shanhm <shanhm1991@163.com>
2019-02-28 18:55:54 +0800

    引入springcloud依赖

--- src/main/java/com/fom/web/FomController.java
+++ src/main/java/com/fom/web/FomController.java
@@ -7,9 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fom.context.FomService;
 
@@ -18,7 +18,7 @@ import com.fom.context.FomService;
  * @author shanhm
  *
  */
-@Controller
+@RestController
 public class FomController {
 
 	@Autowired
