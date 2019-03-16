95b746ee6825e52ccc18330d18f0e87e4e42d267
shanhm <shanhm1991@163.com>
2019-03-16 16:54:10 +0800

    跨域问题

--- src/main/java/com/fom/context/FomConfiguration.java
+++ src/main/java/com/fom/context/FomConfiguration.java
@@ -15,6 +15,9 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.fom.pool.PoolListener;
 
@@ -47,6 +50,23 @@ public class FomConfiguration implements ServletContextInitializer {
 		listener.setListener(new FomContextListener());
 		listener.setOrder(5);
 		return listener;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig()); 
+		return new CorsFilter(source);
+	}
+	
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.setAllowCredentials(true);//这两句不加不能跨域上传文件，
+		corsConfiguration.setMaxAge(3600l);//加上去就可以了
+		return corsConfiguration;
 	}
 
 	@Bean
