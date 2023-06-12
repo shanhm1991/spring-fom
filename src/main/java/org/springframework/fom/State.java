a8908f5c896635c87b0d57aa6535a83af0deb0fa
shanhm1991@163.com <shanhm1991@163.com>
2023-06-12 19:24:08 +0800

    静态资源路径修改

--- src/main/java/org/springframework/fom/State.java
+++ src/main/java/org/springframework/fom/State.java
@@ -10,27 +10,27 @@ public enum State {
 	/**
 	 * 初始化
 	 */
-	INITED("images/inited.png", "inited"),
+	INITED("fom/images/inited.png", "inited"),
 
 	/**
 	 * 正在运行
 	 */
-	RUNNING("images/running.png", "running"),
+	RUNNING("fom/images/running.png", "running"),
 
 	/**
 	 * 等待定时周期
 	 */
-	SLEEPING("images/sleeping.png", "waiting for next time"),
+	SLEEPING("fom/images/sleeping.png", "waiting for next time"),
 
 	/**
 	 * 正在停止
 	 */
-	STOPPING("images/stopping.png", "stopping"),
+	STOPPING("fom/images/stopping.png", "stopping"),
 
 	/**
 	 * 已经停止
 	 */
-	STOPPED("images/stopped.png", "stopped");
+	STOPPED("fom/images/stopped.png", "stopped");
 
 	private String src;
 
